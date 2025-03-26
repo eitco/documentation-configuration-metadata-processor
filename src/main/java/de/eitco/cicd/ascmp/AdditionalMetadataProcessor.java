24ac190deb825a38a79a4580da70672e3af80e78
CMerkel <cmerkel@eitco.de>
2025-03-26 17:07:12 +0100

    Finish project setup

--- src/main/java/de/eitco/cicd/ascmp/AdditionalMetadataProcessor.java
+++ src/main/java/de/eitco/cicd/ascmp/AdditionalMetadataProcessor.java
@@ -11,6 +11,7 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
